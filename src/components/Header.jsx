075d42fad3cdc59b4aaf5dd3aa387e082ca4e65e
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-01-23 21:18:32 +0530

    Header - html added to " header-inner"

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-inner">
+        <span className="logo">Hafiz</span>
+        <div className="links">
+          <a href="" className="link">
+            Home
+          </a>
+          <a href="" className="link">
+            Services
+          </a>
+          <a href="" className="link">
+            Experience
+          </a>
+          <a href="" className="link">
+            Projects
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
