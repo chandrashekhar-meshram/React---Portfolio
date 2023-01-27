96b27b153bb86f69bbad63639457ec0853fc37af
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-01-27 14:36:15 +0530

    html file, servicesExperience html added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import "./style.css";
 // files
 import Header from './components/Header';
 import Banner from './components/Banner';
+import ServicesExperience from './components/ServicesExperience';
 
 export default function App() {
   return (
     <div>
       <Header />      
       <Banner />
+      <ServicesExperience />
     </div>
   );
 }
